771edf49ac9c7e4e74139cc696aa7a90d96b723b
shianqi <2573926588@qq.com>
2022-10-08 22:17:34 +0800

    day01

--- day01/demo02.c
+++ day01/demo02.c
@@ -5,7 +5,15 @@ int main()
     /* code */
     char c1 = 'A';
     int a = c1;
+    short b = c1;
+    long c = c1;
     printf("A的ASCII是%d\n",a);
+    printf("char的大小是%d字节\n", sizeof(c1));
+    printf("short的大小是%d字节\n", sizeof(b));
+    printf("int的大小是%d字节\n", sizeof(a));
+    printf("long的大小是%d字节\n", sizeof(c));
+    printf("float的大小是%d字节\n", sizeof(float));
+    printf("double的大小是%d字节\n", sizeof(double));
     system("pause");
     return 0;
 }
