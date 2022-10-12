2ee203f545c088bdfcd436a312f3e25ac500a945
shianqi <2573926588@qq.com>
2022-10-12 14:20:34 +0800

    day01

--- day02/demo04..c
+++ day02/demo04..c
@@ -7,9 +7,12 @@ int main()
     // 定义字符串
     char str1[] = "hello";
     char str2[] = {'h','e','l','l','o','\0'};
+    char str3[] = {'h','e','l','l','o'};
     // int len = strlen(str1);
-    int len = strlen(str2);
+    // int len = strlen(str2);
+    int len = strlen(str3);
     printf("%d\n",len);
+    // printf("%d\n",len);
     printf("=============================\n");
     for (int i = 0; i < strlen(str2); i++)
     {
