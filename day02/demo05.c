d02f4317aee6ea89a04d179eb76dd861d938b1fc
shianqi <2573926588@qq.com>
2022-10-13 15:26:34 +0800

    day02

--- day02/demo05.c
+++ day02/demo05.c
@@ -14,5 +14,18 @@ int main()
     printf("\a\a\a");
     printf("%c\n",'\101');
     printf("%c\n",'\x30');
+    int input = 0;
+    printf("请输入:>");
+    scanf("%d",&input);
+    if (input == 1)
+    {
+        /* code */
+        printf("毕业好offer~~~");
+    }
+    else{
+        /* code */
+        printf("毕业烤红薯~~~");
+    }
+    
     return 0;
 }
