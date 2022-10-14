caf7c051b7f400f111e73fa1fc07724fa0f82c8a
shianqi <2573926588@qq.com>
2022-10-14 13:08:37 +0800

    day02

---
+++ day02/demo08.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    /* code */
+    int array1[10] = {1,2,3,4,5,6,7,8,9,0};
+    // 可以不完全初始化。没有初始化的元素默认为\0。
+    char array2[5] = {'a','b','c'}; 
+    for (int i = 0; i < 4; i++)
+    {
+        /* code */
+        printf("%c\n",array2[i]);
+    }
+    
+    return 0;
+}
