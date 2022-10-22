8fbdf13cec9f0fbfccac64aa098b2d4611ac0bda
shianqi <2573926588@qq.com>
2022-10-22 22:49:55 +0800

    day04

--- day04/demo02.c
+++ day04/demo02.c
@@ -5,7 +5,7 @@ int main()
 {
     /* code */
     // getchar或取一个字符输入 返回输入字符的ASCII码值整型值
-    // putchar换行输出一个字符
+    // putchar在终端中换行输出一个字符
     int ch = getchar();
     printf("%c\n",ch);
     putchar(ch);
