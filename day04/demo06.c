4c727fbd7995cc87a93ce3ed769ff4137d2839ab
shianqi <2573926588@qq.com>
2022-10-24 15:46:08 +0800

    day04

---
+++ day04/demo06.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    /* code */
+    int i = 1;
+    do
+    {
+        /* code */
+        printf("%d\n",i);
+        i++;
+    } while (i <= 10);
+    
+    return 0;
+}
