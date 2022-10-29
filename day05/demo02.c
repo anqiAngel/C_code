5a739617c6f710797f1ccea5840d6b1e93b8e28a
shianqi <2573926588@qq.com>
2022-10-29 15:40:55 +0800

    day05

--- day05/demo02.c
+++ day05/demo02.c
@@ -3,9 +3,19 @@
 #include <windows.h>
 #include <math.h>
 
+void num_add(int* num){
+    (*num)++;
+}
+
 int main()
 {
     /* code */
-    
+    int num = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        /* code */
+        num_add(&num);
+        printf("%d\n",num);
+    }
     return 0;
 }
