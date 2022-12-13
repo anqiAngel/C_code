e22f0d79c0696f8014d825bcb1b5cee7cf052789
shianqi <2573926588@qq.com>
2022-12-13 19:38:57 +0800

    day06

--- day06/demo04.c
+++ day06/demo04.c
@@ -5,13 +5,18 @@ void bubble_sort(int arr[], int size){
     // i趟数 n个数字n-1趟排序
     for (int i = 0;i < size - 1;i++){
         //j控制每趟比较次数
+        int num = 0;
         for (int j = 0;j < size - 1 - i; j++)
-        {
+        {   
             if(arr[j] > arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                num++;
             }
+        }
+        if(num == 0){
+            break;   
         }
         
     }
@@ -21,6 +26,7 @@ int main()
 {
     /* code */
     int arr[10] = {9,8,7,6,5,4,3,2,1,0};
+    //int arr[10] = {0,1,2,3,4,5,6,7,8,9};
     //求数组大小
     int size = sizeof(arr)/sizeof(arr[0]);
     bubble_sort(arr,size);
