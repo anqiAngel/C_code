a1e565cf7300f6d3c710a960003397cf7e0f243e
shianqi <2573926588@qq.com>
2022-12-13 19:21:24 +0800

    day06

---
+++ day06/demo04.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// 冒泡排序
+void bubble_sort(int arr[], int size){
+    // i趟数 n个数字n-1趟排序
+    for (int i = 0;i < size - 1;i++){
+        //j控制每趟比较次数
+        for (int j = 0;j < size - 1 - i; j++)
+        {
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+        
+    }
+}
+
+int main()
+{
+    /* code */
+    int arr[10] = {9,8,7,6,5,4,3,2,1,0};
+    //求数组大小
+    int size = sizeof(arr)/sizeof(arr[0]);
+    bubble_sort(arr,size);
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
