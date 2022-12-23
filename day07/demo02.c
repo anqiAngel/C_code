4217f187bf7529a652ddde0b89b254238dba2884
shianqi <2573926588@qq.com>
2022-12-23 14:38:33 +0800

    day07

--- day07/demo02.c
+++ day07/demo02.c
@@ -3,5 +3,16 @@
 int main()
 {
     /* code */
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int *p = arr;
+    int *pend = p + 9;
+    while (p<=pend)
+    {
+        /* code */
+        printf("%d\n",*p);
+        p++;
+    }
+    printf("%d",&arr[9]-&arr[0]);
+    
     return 0;
 }
