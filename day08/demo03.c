3bc8820e7e42fdcd3b731a9502e9255c23d94d58
shianqi <2573926588@qq.com>
2023-01-28 21:52:41 +0800

    day08

---
+++ day08/demo03.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// 统计一个数二进制形式1的个数
+int one_binary_total1(int num){
+    int count = 0;
+    while (num)
+    {
+        /* code */
+        if(num&1 == 1){ 
+            count++;
+        }
+        num>>=1;
+    }
+    return count;
+    
+}
+int one_binary_total2(int num){
+    int count = 0;
+    while (num)
+    {
+        /* code */
+        if(num&1 == 1){ 
+            count++;
+        }
+        num>>=1;
+    }
+    return count;
+    
+}
+int main()
+{
+    /* code */
+    printf("%d\n",one_binary_total(4));
+    printf("%d\n",one_binary_total(3));
+    printf("%d\n",one_binary_total(7));
+    return 0;
+}
