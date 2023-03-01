e7f4d56a9b24308456234dd6a9ab3a0252be9d3d
shianqi <2573926588@qq.com>
2023-03-01 20:59:46 +0800

    day10

--- 进阶day02/demo03.c
+++ 进阶day02/demo03.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int Add(int a,int b){
+    return a + b;
+}
+int Sub(int a,int b){
+    return a - b;
+}
+
 int main()
 {
     /* code */
-    char a = 67;
-    printf("%c",a);
-    system("pause");
+    // 函数指针数组
+    int (*arr[2])(int,int) = {Add,Sub};    
     return 0;
 }
