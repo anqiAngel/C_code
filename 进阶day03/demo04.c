4953fbcfd5a400de2499027ebcc984ade762ddb9
shianqi <2573926588@qq.com>
2023-03-31 23:33:28 +0800

    进阶day04

--- 进阶day03/demo04.c
+++ 进阶day03/demo04.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+// 字符串左旋
+void left_rotate_string1(char str[], int k)
+{   
+    for (int i = 0; i < k; i++)
+    {
+        /* code */
+        int len = strlen(str);
+        char tmp = *str;
+        for (int j = 0; j < len - 1; j++)
+        {
+            /* code */
+            *(str+j) = *(str+j+1);
+        }
+        *(str + len - 1) = tmp;  
+    }
+    
+}
 
 int main()
 {
     /* code */
+    char str[] = "ABCDE";
+    left_rotate_string1(str,3);
+    printf("%s\n",str);
     return 0;
 }
